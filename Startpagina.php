8be89f4aed0ca11c0e1016845ed165ee75c8eeff
Nikki <s1198053@student.windesheim.nl>
2024-10-23 17:48:02 +0200

    StartPagina

---
+++ Startpagina.php
@@ -0,0 +1,55 @@
+
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Startpagina</title>
+
+    <style>
+        body {
+            text-align: center;
+
+        }
+        .FotoAlmere {
+            height: 500px;
+            background-image: url('img/AlmereMaps.png');
+            background-size: cover;
+            background-position: center;
+        }
+
+        .BeginButton{
+            display: flow;
+            background-color: darkcyan;
+            border: none;
+            color: white;
+            padding: 15px 32px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 32px;
+        }
+
+        .BeginButton:hover {
+            background-color: aqua;
+            color: white;
+        }
+
+        .WindesheimLogo {
+            position: absolute;
+            top: 15%;
+            left: 0;
+            right: 0;
+        }
+
+    </style>
+</head>
+</html>
+<body>
+
+<div class="FotoAlmere"></div>
+<div class="WindesheimLogo" > <img src="img/WindesheimLogo.png" alt="WindesheimLogo" width="300" height="auto"> </div>
+<h1>(Beschrijving van de Routekaart)</h1>
+<input type="submit" value="Begin" class="BeginButton"/>
+
+</body>
