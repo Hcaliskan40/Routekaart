14fbcc97f382bfa50cff2776e84923b6074e1b04
jjasmin2 <146453032+jjasmin2@users.noreply.github.com>
2024-10-23 15:08:30 +0200

    Code voor pagina 1

--- Vraag1.php
+++ Vraag1.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../CSS/Vraag1.css?v=1.0">
+    <link rel="stylesheet" type="text/css" href="./CSS/Vraag1.css?v=1.0">
     <title>Vraag1</title>
 </head>
 <body>
@@ -11,9 +11,8 @@
 <div class="container">
     <div class="header">
         <span class="dot">1</span>
-    <h1 class="titel-balk">Like</h1>
-
-</div>
+        <h1 class="titel-balk">Like</h1>
+    </div>
 
     <div class="content">
         <div class="row">
@@ -35,10 +34,7 @@
     </div>
 </div>
 
-
-
-
-
-
 </body>
 </html>
+
+
