e25fc9e836cca97eddb05c75136ba921df1837b4
brundunn <brandon99@live.nl>
2024-10-23 15:06:34 +0200

    apache port fix

--- popup.php
+++ popup.php
@@ -29,8 +29,8 @@ function generateRoster($connection) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../CSS/image.css" media="screen, projection">
-    <link rel="stylesheet" type="text/css" href="../CSS/popup.css" media="screen, projection">
+    <link rel="stylesheet" type="text/css" href="css/image.css" media="screen, projection">
+    <link rel="stylesheet" type="text/css" href="css/popup.css" media="screen, projection">
     <title>Popup Roster</title>
 </head>
 <body>
