ee242965b6a75f90c0079de0b951a2cf2a728e3f
jjasmin2 <146453032+jjasmin2@users.noreply.github.com>
2024-10-24 13:19:12 +0200

    Vraag 1

--- opdracht1.php
+++ opdracht1.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Like Section</title>
-    <link rel="stylesheet" type="text/css" href="Opdracht.css">
+    <link rel="stylesheet" type="text/css" href="css/Opdracht.css">
 
 </head>
 <body>
@@ -13,26 +13,30 @@
         <span class="dot">1</span>
         <div class="titel-balk"> Like</div> <!-- Tekst die aangeeft dat dit sectie 1 is, met het onderwerp 'Like'. -->
     </div>
-    <p>Hier krijg ik energie van en doe ik graag:</p> <!-- Beschrijving van de inhoud die de gebruiker gaat invullen. -->
+    <p>Hier krijg ik energie van en doe ik graag:</p>
     <div class="input-box">
         <div class="input-item">
             <div class="image-placeholder">+</div> <!-- Terug naar de '+' om aan te geven dat hier iets toegevoegd kan worden. -->
-            <textarea name="message" rows="10" cols="40" placeholder="Bijvoorbeeld: Muziek luisteren"> </textarea>
+            <textarea name="message" rows="10" cols="40" placeholder="Wat vind ik hier leuk aan?"> </textarea>
         </div>
         <div class="input-item">
             <div class="image-placeholder">+</div>
-            <textarea name="message2" rows="3" cols="40" placeholder="Bijvoorbeeld: Tuinieren"></textarea>
+            <textarea name="message2" rows="3" cols="40" placeholder="Wat vind ik hier leuk aan?"></textarea>
         </div>
         <div class="input-item">
             <div class="image-placeholder">+</div>
-            <textarea name="message3" rows="3" cols="40" placeholder="Bijvoorbeeld: Sporten"></textarea>
-        </div>
+            <textarea name="message3" rows="3" cols="40" placeholder="Wat vind ik hier leuk aan?"></textarea>
+
+
     </div>
-    <div class="button-group">
-        <button class="arrow-btn">&#8249;</button> <!-- Linker navigatieknop om terug te gaan. -->
-        <button class="confirm-btn">Bevestigen</button> <!-- Knop om de invoer te bevestigen. -->
-        <button class="arrow-btn">&#8250;</button> <!-- Rechter navigatieknop om verder te gaan. -->
-    </div>
+        <div class="button-group">
+            <button class="arrow-btn">&#8249;</button><!-- Linker navigatieknop om terug te gaan. -->
+            <button class="button-1" role="button">Button 1</button>
+            <button class="arrow-btn">&#8250;</button> <!-- Rechter navigatieknop om verder te gaan. -->
 </div>
+
+
+
+
 </body>
 </html>
