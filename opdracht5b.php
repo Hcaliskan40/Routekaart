139ad6a3b0ee9e167ef9b4845fe82adfe64321bf
Hcaliskan <52010720+Hcaliskan40@users.noreply.github.com>
2024-11-07 11:59:09 +0100

    5b

--- opdracht5b.php
+++ opdracht5b.php
@@ -10,7 +10,7 @@
 <div class="container">
     <div class="header">
         <span class="dot">6</span>
-        <div class="titel-balk">Opleidingen</div> <!-- Tekst die aangeeft dat dit sectie 1 is, met het onderwerp 'Like'. -->
+        <div class="titel-balk">Opleidingen</div>
     </div>
     <b>Dit is mijn TOP 3 sectoren:</b>
     <p>Deze beroepen passen hierbij voor mij:</p>
@@ -18,22 +18,22 @@
         <div class="input-item">
             <div class="input-number">1</div>
             <input type="text" class="dotted-input" placeholder="Beroepen">
-            <input type="text" class="rounded-input" placeholder="Opleiding(en)">
+            <input type="text" class="rounded-input" placeholder="Waarom dit beroep?">
         </div>
         <div class="input-item">
             <div class="input-number">2</div>
             <input type="text" class="dotted-input" placeholder="Beroepen">
-            <input type="text" class="rounded-input" placeholder="Opleiding(en)">
+            <input type="text" class="rounded-input" placeholder="Waarom dit beroep?">
         </div>
         <div class="input-item">
             <div class="input-number">3</div>
             <input type="text" class="dotted-input" placeholder="Beroepen">
-            <input type="text" class="rounded-input" placeholder="Opleiding(en)">
+            <input type="text" class="rounded-input" placeholder="Waarom dit beroep?">
         </div>
         <div class="button-group">
-            <button class="arrow-btn">&#8249;</button><!-- Linker navigatieknop om terug te gaan. -->
+            <button class="arrow-btn">&#8249;</button>
             <button class="button-1" role="button">Button 1</button>
-            <button class="arrow-btn">&#8249;</button> <!-- Rechter navigatieknop om verder te gaan. -->
+            <button class="arrow-btn">&#8250;</button>
         </div>
     </div>
 
