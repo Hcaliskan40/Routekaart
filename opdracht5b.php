63ce439000f142e6da4e489ffda30490db315f12
Hcaliskan <52010720+Hcaliskan40@users.noreply.github.com>
2024-11-07 11:59:52 +0100

    5b

--- opdracht5b.php
+++ opdracht5b.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Opleidingen</title>
-    <link rel="stylesheet" type="text/css" href="css/opdracht6.css">
+    <link rel="stylesheet" type="text/css" href="css/opdracht5b.css">
 </head>
 <body>
 <div class="container">
