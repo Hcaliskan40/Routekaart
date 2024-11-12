15a8f6cf87ae8b14b4486e38a4bb37ad4314e220
Hcaliskan <52010720+Hcaliskan40@users.noreply.github.com>
2024-11-12 15:55:46 +0100

    Opdracht 7

--- opdracht7.php
+++ opdracht7.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Verlanglijst - Ronde Checkboxes</title>
-    <link rel="stylesheet" type="text/css" href="css/opdracht7_ronde_checkbox.css">
+    <link rel="stylesheet" type="text/css" href="css/opdracht7.css">
 </head>
 <body>
 <div class="container">
