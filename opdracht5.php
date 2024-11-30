9ac3520e7df2928e7d45acf79d177205c5d21313
Joshua <joshuaethanwilms@gmail.com>
2024-11-30 14:41:27 +0100

    opdracht 5

--- opdracht5.php
+++ opdracht5.php
@@ -17,54 +17,71 @@
     <div class="options">
         <label class="option-item">
             <input type="checkbox" class="option-checkbox"> Bouw / Architectuur
+            <button class="info-button" title="More information about Bouw / Architectuur">i</button>
         </label>
         <label class="option-item">
             <input type="checkbox" class="option-checkbox"> Communicatie / Media
+            <button class="info-button" title="More information about Bouw / Architectuur">i</button>
         </label>
         <label class="option-item">
             <input type="checkbox" class="option-checkbox"> Economie / Financiën
+            <button class="info-button" title="More information about Economie / Financiën">i</button>
         </label>
         <label class="option-item">
             <input type="checkbox" class="option-checkbox"> Facilitaire dienstverlening / Horeca
+            <button class="info-button" title="More information about Facilitaire dienstverlening / Horeca">i</button>
         </label>
         <label class="option-item">
             <input type="checkbox" class="option-checkbox"> Fashion / Design
+            <button class="info-button" title="More information about Fashion / Design">i</button>
         </label>
         <label class="option-item">
             <input type="checkbox" class="option-checkbox"> Gezondheidszorg / Life science
+            <button class="info-button" title="More information about Gezondheidszorg / Life science">i</button>
         </label>
         <label class="option-item">
             <input type="checkbox" class="option-checkbox"> ICT / IT
+            <button class="info-button" title="More information about ICT / IT">i</button>
         </label>
         <label class="option-item">
             <input type="checkbox" class="option-checkbox"> Juridisch / Recht
+            <button class="info-button" title="More information about Juridisch / Recht">i</button>
         </label>
         <label class="option-item">
             <input type="checkbox" class="option-checkbox"> Kunst / Cultuur / Entertainment
+            <button class="info-button" title="More information about Kunst / Cultuur / Entertainment">i</button>
         </label>
         <label class="option-item">
             <input type="checkbox" class="option-checkbox"> Management / Ondernemen
+            <button class="info-button" title="More information about Management / Ondernemen">i</button>
         </label>
         <label class="option-item">
             <input type="checkbox" class="option-checkbox"> Natuur (planten, dieren en milieu)
+            <button class="info-button" title="More information about Natuur (planten, dieren en milieu)">i</button>
         </label>
         <label class="option-item">
             <input type="checkbox" class="option-checkbox"> Onderwijs
+            <button class="info-button" title="More information about Onderwijs">i</button>
         </label>
         <label class="option-item">
             <input type="checkbox" class="option-checkbox"> Sport / Voeding / Beweging
+            <button class="info-button" title="More information about Sport / Voeding / Beweging">i</button>
         </label>
         <label class="option-item">
             <input type="checkbox" class="option-checkbox"> Techniek
+            <button class="info-button" title="More information about Techniek">i</button>
         </label>
         <label class="option-item">
             <input type="checkbox" class="option-checkbox"> Toerisme / Recreatie
+            <button class="info-button" title="More information about Toerisme / Recreatie">i</button>
         </label>
         <label class="option-item">
             <input type="checkbox" class="option-checkbox"> Transport / Logistiek
+            <button class="info-button" title="More information about Transport / Logistiek">i</button>
         </label>
         <label class="option-item">
             <input type="checkbox" class="option-checkbox"> Veiligheid
+            <button class="info-button" title="More information about Veiligheid">i</button>
         </label>
     </div>
 
