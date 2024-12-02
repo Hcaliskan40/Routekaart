6047f1f52a2fa3e87907b8f35b86696d9866f27d
brundunn <brandon99@live.nl>
2024-12-02 11:34:47 +0100

    pdf generating speed

--- gekozenfotos.php
+++ gekozenfotos.php
@@ -40,11 +40,134 @@ $selectedImagesOpdracht4 = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chosen Images</title>
-    <link rel="stylesheet" href="css/styles.css">
-    <link rel="stylesheet" href="css/pdf.css">
+<!--    <link rel="stylesheet" href="css/styles.css">-->
+<!--    <link rel="stylesheet" href="css/pdf.css">-->
+    <style>
+        /* Contents of styles.css */
+        .image-container {
+            display: flex;
+            justify-content: space-around;
+            margin-top: 50px;
+        }
+
+        .image-button {
+            background: none;
+            border: none;
+            padding: 0;
+            cursor: pointer;
+        }
+
+        .image-button img {
+            width: 150px;
+            height: 150px;
+            border: 1px solid #000;
+        }
+
+        /* Contents of pdf.css */
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f4;
+        }
+
+        .container {
+            width: 80%;
+            margin: 0 auto;
+            padding: 20px;
+        }
+
+        #logo {
+            display: block;
+            margin: 0 auto;
+            width: 200px;
+            height: auto;
+        }
+
+        .intro {
+            text-align: center;
+            margin: 20px 0;
+        }
+
+        .header1 {
+            display: flex;
+            align-items: center;
+            font-size: 2em;
+            font-weight: bold;
+            color: #b1d249;
+            padding-bottom: 15px;
+        }
+
+        .titel-balk1 {
+            background-color: #b1d249;
+            color: white;
+            font-size: 30px;
+            flex-grow: 0.8;
+            padding: 10px 20px;
+        }
+
+        .dot1 {
+            height: 75px;
+            width: 75px;
+            background-color: #b1d249;
+            border-radius: 50%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-size: 30px;
+            color: white;
+            font-family: "Roboto Thin";
+        }
+
+        h2 {
+            font-size: 1.2em;
+            color: #333;
+            margin-top: 20px;
+            border-bottom: 1px solid #ccc;
+            padding-bottom: 5px;
+        }
+
+        .chosen-images {
+            display: flex;
+            flex-wrap: wrap;
+            gap: 10px;
+            margin-top: 10px;
+        }
+
+        .image-item {
+            flex: 1 1 calc(33.333% - 10px);
+            box-sizing: border-box;
+            padding: 5px;
+            border: 1px solid #ccc;
+            background-color: #fafafa;
+            text-align: center;
+        }
+
+        .image-item img {
+            max-width: 100%;
+            height: auto;
+            display: block;
+            margin: 0 auto;
+        }
+
+        /*@media print {*/
+        /*    body {*/
+        /*        background-color: #fff;*/
+        /*    }*/
+
+        /*    .container {*/
+        /*        width: 100%;*/
+        /*        box-shadow: none;*/
+        /*    }*/
+
+        /*    h2 {*/
+        /*        page-break-before: always;*/
+        /*    }*/
+        /*}*/
+    </style>
 </head>
 <body>
-<img id="logo" src="http://localhost:80/Routekaart/img/WindesheimLogo.png" alt="Windesheim Logo">
+<img id="logo" src="img/WindesheimLogo.png" alt="Windesheim Logo">
 <div class="intro">
     <h1>Studiekeuze Routekaart</h1>
     <p>Bedankt voor het invullen van jouw studiekeuzevragen. Hier vind je een overzicht van jouw antwoorden en keuzes.
--- generate-pdf.php
+++ generate-pdf.php
@@ -4,10 +4,15 @@ require 'vendor/autoload.php';
 use Dompdf\Dompdf;
 use Dompdf\Options;
 
+
+// Increase memory limit
+ini_set('memory_limit', '16');
+
 // Initialize Dompdf with options
 $options = new Options();
 $options->set('isHtml5ParserEnabled', true);
 $options->set('isRemoteEnabled', true);
+$options->set('defaultFont', 'DejaVu Sans'); // Use a default font to speed up rendering
 
 $dompdf = new Dompdf($options);
 
@@ -20,7 +25,7 @@ $html = ob_get_clean();
 $html = preg_replace('/href="css\//', 'href="http://localhost:80/Routekaart/css/', $html);
 
 // Ensure image paths are absolute URLs
-$html = preg_replace('/src="img\//', 'src="http://localhost:80/Routekaart/img/', $html);
+//$html = preg_replace('/src="img\//', 'src="http://localhost:80/Routekaart/img/', $html);
 
 $dompdf->loadHtml($html);
 
@@ -30,5 +35,7 @@ $dompdf->setPaper('A4', 'portrait');
 // Render the HTML as PDF
 $dompdf->render();
 
+// Clean (erase) the output buffer and turn off output buffering
+ob_end_clean();
 // Output the generated PDF to Browser
 $dompdf->stream("chosen_images.pdf", ["Attachment" => 1]);
