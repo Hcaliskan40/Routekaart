e2c0a9e4d06d22fcc45cec5799f22caeaa8cf006
Hcaliskan <52010720+Hcaliskan40@users.noreply.github.com>
2024-12-18 14:34:18 +0100

    pdf

--- generate-pdf.php
+++ generate-pdf.php
@@ -7,6 +7,21 @@ require 'vendor/autoload.php';
 use Dompdf\Dompdf;
 use Dompdf\Options;
 
+function ImageToDataUrl($filename) {
+    if(!file_exists($filename))
+        return "";
+
+    $mime = mime_content_type($filename);
+    if($mime === false)
+        return "";
+
+    $raw_data = file_get_contents($filename);
+    if(empty($raw_data))
+        return "";
+
+    return "data:{$mime};base64," . base64_encode($raw_data);
+}
+
 // Stel opties in voor Dompdf
 $options = new Options();
 $options->set('isRemoteEnabled', true); // Voor externe bronnen zoals afbeeldingen
@@ -20,8 +35,29 @@ ob_start();
 require 'gekozenfotos.php'; // Zorg ervoor dat gekozenfotos.php correcte HTML retourneert
 $html = ob_get_clean();
 
+$doc = new DOMDocument();
+libxml_use_internal_errors(true);
+$doc->loadHTML($html);
+
+$tags = $doc->getElementsByTagName('img');
+foreach ($tags as $tag) {
+    $imgurl = $tag->getAttribute('src');
+    $newImageBase64 = ImageToDataUrl($imgurl);
+    $tag->setAttribute('src', $newImageBase64);
+}
+$atags = $doc->getElementsByTagName('a');
+foreach($atags as $atag) {
+    if($atag->textContent == "Download PDF") {
+        $atag->parentNode->removeChild($atag);
+    }
+}
+$html = $doc->saveHTML();
+
+// De html is goed hier
+
+
 // Zorg ervoor dat de afbeelding-paden absolute URL's zijn
-$html = preg_replace('/src="img\//', 'src="http://localhost:8080/routekaart/img/', $html);
+//$html = preg_replace('/src="img\//', 'src="http://localhost/routekaart/img/', $html);
 
 // Laad de HTML in Dompdf
 $dompdf->loadHtml($html);
