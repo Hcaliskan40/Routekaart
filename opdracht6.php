7fda375775a5e90021981b7c22e2812007731b4b
brundunn <brandon99@live.nl>
2024-12-20 00:23:08 +0100

    opdracht6.php cleaned code and added documentation

--- opdracht6.php
+++ opdracht6.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
 
+/**
+ * Retrieve messages from session or set to an empty string if not set.
+ */
 $message5a = $_SESSION['message5a'] ?? '';
 $message5b = $_SESSION['message5b'] ?? '';
 $message5c = $_SESSION['message5c'] ?? '';
@@ -26,24 +29,24 @@ $message6c = $_SESSION['message6c'] ?? '';
     <b>Dit is mijn TOP 3 beroepen:</b>
     <p>Deze opleidingen passen hierbij:</p>
     <div class="input-box">
-        <div class="input-item">
-            <div class="input-number">1</div>
-            <input type="text" class="dotted-input" placeholder="Beroepen" value="<?php echo htmlspecialchars($message5a, ENT_QUOTES, 'UTF-8'); ?>">
-            <input id="message6a" type="text" class="rounded-input" placeholder="Welke opleiding heb je nodig?" value="<?php echo htmlspecialchars($message6a, ENT_QUOTES, 'UTF-8'); ?>" oninput="saveText('message6a')">
-        </div>
-        <div class="input-item">
-            <div class="input-number">2</div>
-            <input type="text" class="dotted-input" placeholder="Beroepen" value="<?php echo htmlspecialchars($message5b, ENT_QUOTES, 'UTF-8'); ?>">
-            <input id="message6b" type="text" class="rounded-input" placeholder="Welke opleiding heb je nodig?" value="<?php echo htmlspecialchars($message6b, ENT_QUOTES, 'UTF-8'); ?>" oninput="saveText('message6b')">
-        </div>
-        <div class="input-item">
-            <div class="input-number">3</div>
-            <input type="text" class="dotted-input" placeholder="Beroepen" value="<?php echo htmlspecialchars($message5c, ENT_QUOTES, 'UTF-8'); ?>">
-            <input id="message6c" type="text" class="rounded-input" placeholder="Welke opleiding heb je nodig?" value="<?php echo htmlspecialchars($message6c, ENT_QUOTES, 'UTF-8'); ?>" oninput="saveText('message6c')">
-        </div>
+        <?php
+        $messages5 = [$message5a, $message5b, $message5c];
+        $messages6 = [$message6a, $message6b, $message6c];
+        for ($i = 0; $i < 3; $i++): ?>
+            <div class="input-item">
+                <div class="input-number"><?php echo $i + 1; ?></div>
+                <input type="text" class="dotted-input" placeholder="Beroepen" value="<?php echo htmlspecialchars($messages5[$i], ENT_QUOTES, 'UTF-8'); ?>">
+                <input id="message6<?php echo chr(97 + $i); ?>" type="text" class="rounded-input" placeholder="Welke opleiding heb je nodig?" value="<?php echo htmlspecialchars($messages6[$i], ENT_QUOTES, 'UTF-8'); ?>" oninput="saveText('message6<?php echo chr(97 + $i); ?>')">
+            </div>
+        <?php endfor; ?>
     </div>
 
     <script>
+        /**
+         * Save the text input value to the session via an AJAX POST request.
+         *
+         * @param {string} id - The ID of the input element whose value is to be saved.
+         */
         function saveText(id) {
             const value = document.getElementById(id).value;
             const xhr = new XMLHttpRequest();
